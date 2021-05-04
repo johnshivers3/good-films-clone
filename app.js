48701da6ccc0bdce7f684f371d06f6afb18495ff
Ethan Kaseff <ethan@kaseff.com>
2021-05-04 17:57:10 -0500

    get movie page with movies on it

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const reviewsRouter = require('./routes/reviews')
+const moviesRouter = require('./routes/movies')
 const { sessionSecret } = require('./config');
 const { restoreUser } = require('./auth');
 
@@ -42,7 +43,8 @@ store.sync();
 app.use(restoreUser);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/reviews', reviewsRouter)
+app.use('/reviews', reviewsRouter);
+app.use('/movies', moviesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
