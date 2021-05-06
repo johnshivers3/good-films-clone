7cf4b8d1cef0b97327fbe15768c295131ce12a02
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-05-06 17:40:14 -0500

    movie.js added review

--- public/javascripts/movie.js
+++ public/javascripts/movie.js
@@ -23,18 +23,26 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('create-review').addEventListener('click', async (event) =>{
         event.preventDefault();
 
-        const collectionId = document.getElementById('collectionSelect').value;
+        const rating = document.getElementById('rating').value;
+        const content = document.getElementById('content').value;
+        const userId = document.getElementById('userId').value;
         const movieId = document.getElementById('movieId').value;
 
-        const hello = await fetch(`/profile/${collectionId}/${movieId}`, {
+        const hello = await fetch(`/movies`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
+            },
+            body: {
+                rating,
+                content,
+                userId,
+                movieId
             }
         })
 
-        const div = document.getElementById('addToCollectionsDiv');
-        div.innerHTML = '';
+        console.log("hello")
+
     });
 
 
