c71a15272d4d8e568868147f01b0528cee949399
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-05-06 17:34:11 -0500

    1

--- public/javascripts/movie.js
+++ public/javascripts/movie.js
@@ -20,5 +20,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+    document.getElementById('create-review').addEventListener('click', async (event) =>{
+        event.preventDefault();
 
-})
+        const collectionId = document.getElementById('collectionSelect').value;
+        const movieId = document.getElementById('movieId').value;
+
+        const hello = await fetch(`/profile/${collectionId}/${movieId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const div = document.getElementById('addToCollectionsDiv');
+        div.innerHTML = '';
+    });
+
+
+
+});
